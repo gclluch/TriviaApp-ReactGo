362d25e29320e0dd31498fa565f1ecd331b08504
gclluch <gclluch@gmail.com>
2024-02-16 09:31:54 -0800

    button

--- frontend/src/MainMenu.js
+++ frontend/src/MainMenu.js
@@ -6,8 +6,8 @@ function MainMenu() {
   return (
     <div className="menu">
     <h1 className="welcome-heading">Welcome to CapTrivia</h1>
-      <Link to="/singleplayer"><button>Single Player</button></Link>
-      <Link to="/multiplayer"><button>Start Multiplayer Game</button></Link>
+      <Link to="/singleplayer"><button className="menu-button">Single Player</button></Link>
+      <Link to="/multiplayer"><button className="menu-button">Multiplayer</button></Link>
     </div>
   );
 }
